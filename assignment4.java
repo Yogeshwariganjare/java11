52985b04846af34b24c542e4792797d9d11d4f34
Yogeshwariganjare <93467255+Yogeshwariganjare@users.noreply.github.com>
2021-11-16 07:28:53 +0530

    Update assignment4.java

--- assignment4.java
+++ assignment4.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 		public class Assignment4 {
 
 			public static void main(String[] args) {
-				var path="C:\\Users\\yogeshwari\\Documents\\java11\\student2.txt ";
+				var path="C:\Users\irg\Downloads";
 				
 				
 				try
